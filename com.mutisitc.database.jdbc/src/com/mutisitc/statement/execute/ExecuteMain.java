0fd509750cb65e0f2472df435b5718cadd40e235
mutistic-client <mutistic@qq.com>
2018-09-26 11:02:21 +0800

    七、使用Statement.execute方法实现数据操作：实现更新、删除操作

---
+++ com.mutisitc.database.jdbc/src/com/mutisitc/statement/execute/ExecuteMain.java
@@ -0,0 +1,34 @@
+package com.mutisitc.statement.execute;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.mutisitc.utils.JDBCUtil;
+import com.mutisitc.utils.PrintUtil;
+
+/**
+ * @program 使用Statement.execute接口实现数据操作
+ * @description 
+ * @author mutisitic
+ * @date 2018年9月26日
+ */
+public class ExecuteMain {
+	public static void main(String[] args) {
+		PrintUtil.one("使用Statement.execute接口实现数据操作：");
+		try {
+			Statement statement = JDBCUtil.createStatement();
+			
+			// 使用Statement.execute接口实现数据新增
+			ExecuteByInsert.mainByInsert(statement);
+			// 使用Statement.execute接口实现数据删除
+			ExecuteByDelete.mainByDelete(statement);
+			// 使用Statement.execute接口实现数据更新
+			ExecuteByUpdate.mainByUpdate(statement);
+			
+			JDBCUtil.close(statement);
+		} catch (SQLException e) {
+			PrintUtil.err("使用Statement.execute接口实现，打印异常堆栈信息：");
+			e.printStackTrace();
+		}
+	}
+}
---
+++ com.mutisitc.database.jdbc/src/com/mutisitc/statement/executeupdate/ExecuteUpdateMain.java
@@ -0,0 +1,5 @@
+package com.mutisitc.statement.executeupdate;
+
+public class ExecuteUpdateMain {
+
+}
